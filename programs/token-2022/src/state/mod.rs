661cb3d2472ab45bd443dbd1438d5dfcbdf2e6c4
Alexander Odegov <alexander.odegov@protonmail.com>
2026-02-09 11:59:57 +0400

    Fmt

--- programs/token-2022/src/state/mod.rs
+++ programs/token-2022/src/state/mod.rs
@@ -1,11 +1,11 @@
 mod account_state;
 mod mint;
 mod multisig;
-mod token;
 mod pausable_config;
+mod token;
 
 pub use account_state::*;
 pub use mint::*;
 pub use multisig::*;
-pub use token::*;
 pub use pausable_config::*;
+pub use token::*;
